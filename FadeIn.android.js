4153bca198345770c65ffa6a10a49a3ed373208b
Marc Fawzi <marc.fawzi@gmail.com>
2017-04-27 04:05:55 -0700

    fixed warning about resizeMode being copied to style prop for the wrapping View and made staggering delay as an option

--- FadeIn.android.js
+++ FadeIn.android.js
@@ -1,26 +1,72 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { Animated, StyleSheet, View } from 'react-native';
+import TimerMixin from 'react-timer-mixin';
+
+import reactMixin from 'react-mixin';
+import cloneReferencedElement from 'react-clone-referenced-element';
 
 const onlyChild = React.Children.only;
 
 export default class FadeIn extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      placeholderContainerOpacity: new Animated.Value(1),
+    };
+  }
 
   render() {
-    let image = onlyChild(this.props.children);
+    let image = cloneReferencedElement(onlyChild(this.props.children), {
+      ref: component => { this._image = component; },
+      onLoadEnd: this._onLoadEnd,
+    });
+
+    let imageStyle = StyleSheet.flatten(image.props.style);
+
+    // `delete` doesn't work in JSCore? 
+    let imagePortableStyle = {}
+    for (let a in imageStyle) {
+      if (a !== "resizeMode") {
+        imagePortableStyle[a] = imageStyle[a]
+      }
+    };
 
     return (
       <View {...this.props}>
-        <View style={styles.placeholderContainer}>
-          <View style={[image.props.style, styles.placeholder, this.props.placeholderStyle]}>
+        {image}
+
+        <Animated.View style={[styles.placeholderContainer, {opacity: this.state.placeholderContainerOpacity}]}>
+          <View style={[imagePortableStyle, styles.placeholder, this.props.placeholderStyle]}>
             {this.props.renderPlaceholderContent}
           </View>
-        </View>
-
-        {image}
+        </Animated.View>
       </View>
     );
   }
+
+  _onLoadEnd = () => {
+    /* NOTE(brentvatne): If we animate in immediately when the onLoadEvent event
+       fires, there are two unwanted consequences:
+     1. Animation feels janky - not entirely sure why that is
+       (handled with minimumWait)
+     2. [Optional] Many images finish loading in the same frame for some reason, and in my
+       opinion it looks better when the images fade in separately
+       (handled with staggerNonce) */
+
+    const minimumWait = 100;
+    const staggerNonce = this.props.randomDelay ? 200 * Math.random() : 0;
+
+    this.setTimeout(() => {
+      Animated.timing(this.state.placeholderContainerOpacity, {
+        toValue: 0,
+        duration: 350,
+      }).start();
+    }, minimumWait + staggerNonce);
+  };
 }
+
+reactMixin(FadeIn.prototype, TimerMixin);
 
 let styles = StyleSheet.create({
   placeholderContainer: {
